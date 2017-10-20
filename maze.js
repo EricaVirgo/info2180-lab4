9fd2b3b1bb6e92f9bbc68514a887efb2e10371c2
EricaVirgo <virgoerica3@gmail.com>
2017-10-20 13:40:01 -0500

    Third commit - Exercise 3

--- maze.js
+++ maze.js
@@ -2,20 +2,46 @@
 //"use strict";
 
 window.onload = function(){
+
+	status = document.getElementById("status");
 	start = document.getElementById("start");
 	end = document.getElementById("end");
-
-	boundary1 = document.getElementById("boundary1");
-	
-	boundary1.addEventListener("mouseover", function(element){
-		boundary1.classList.add("youlose");
-	})
-	
 	boundaries = document.querySelectorAll(".boundary:not(.example)");
-	for ( b=0; b<boundaries.length ; b++){
-		boundaries[b].addEventListener("mouseover", function(element){
-		this.classList.add("youlose");
-	})
+
+	touchedBoundary = false;
+
+
+	function touched(){
+		for ( b= 0; b<boundaries.length ; b++){ 
+			if (boundaries[b].classList.contains("youlose")){
+				status = document.getElementById("status").innerHTML = "You have Lost"
+				touchedBoundary = true;
+			} 
+		}
 	}
+
+	function instant(){
+		for ( b=0; b<boundaries.length ; b++){
+			boundaries[b].addEventListener("mouseover", function(element){
+				for ( b=0; b<boundaries.length ; b++)
+				boundaries[b].classList.add("youlose");
+				status = document.getElementById("status").innerHTML = "You have Lost"
+
+			})
+		}
+	}
+	instant();
+	touched();
+//Exercise 1 - unnecessary
+	//boundary1 = document.getElementById("boundary1");
+	//boundary1.addEventListener("mouseover", function(element){
+	//	boundary1.classList.add("youlose");
+	//})
+
+	end.addEventListener("mouseover", function(e){
+		if ( touchedBoundary  == false){
+			alert( "You win!" );
+		}
+	})
 }
 
